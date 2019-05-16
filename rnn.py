bb439686133085b2ada4cca9b884832461fabe35
razmik <melikbekyan@yahoo.com>
2019-05-16 18:10:36 +0400

    adding backpropagation

--- rnn.py
+++ rnn.py
@@ -103,6 +103,50 @@ class RNN:
     def calculate_loss(self, predicted_probabilities: np.ndarray, labels: np.ndarray) -> float:
         return -sum(np.log(p[labels[i]]) for i, p in enumerate(predicted_probabilities))
 
+    def calculate_h_grad_wrt_z(self, h_t: np.ndarray) -> np.ndarray:
+        """
+        Calculates state gradient w.r.t. z, dh / dz.
+        Note that h_{t} = tanh(z_{t}) or h_{t} = sigmoid(z_{t})"""
+        return 1 - h_t ** 2 if self.non_linearity == 'tanh' else h_t * (1 - h_t)
+
+    def calculate_loss_grad_wrt_z(self, dh_t: np.ndarray, states: np.ndarray, t: int):
+        """
+        Calculates dloss_{t} / dz_{k} for all k from 1 to t. Please note that h_{t} = f(z_{t})
+
+        :param dh_t: dloss_{t} / dh_{t}, aka loss gradient w.r.t state for the same time index t
+        :param states: array of states
+        :param t: time index
+        :return: list of arrays, where each array is the calculated gradient:
+                 [dloss_{t} / dz_{t}, dloss_{t} / dz_{t - 1}, ..., dloss_{t} / dz_{1}]
+        """
+
+        # dl / dz = (dl / dh) * (dh / dz)
+        dz_t = dh_t * self.calculate_h_grad_wrt_z(states[t])
+
+        grads = [dz_t]
+        for i in reversed(range(1, t + 1)):
+            # dl / dz_(t-1) = (dl / dz_{t})(dz_{t} / dh_{t-1}) * (dh_{t-1) / dz_{t-1})
+            dz_i = np.dot(self.w_hh, grads[-1]) * self.calculate_h_grad_wrt_z(states[i - 1])
+            grads.append(dz_i)
+        return grads
+
+    def calculate_w_hh_grad(self, dh_t: np.ndarray, states: np.ndarray, t: int):
+        """
+        Calculates dloss_{t} / dd_whh, aka gradient of loss at time t with respect to W_hh.
+        :param dh_t: dloss_{t} / dh_{t}, aka gradient of loss at time t with respect to h_{t}
+        :param states_gradients: the array of state gradients, where each item represents the state
+                                 gradient with respect to its previous state
+        :param t: time index
+        :return: the gradient of loss w.r.t. W_hh
+        """
+        dw_hh = np.zeros_like(self.w_hh)
+
+        tmp = dh_t * (1 - states[t] ** 2)
+        for i in reversed(range(1, t + 1)):
+            dw_hh += np.outer(tmp, states[i - 1])
+            tmp = np.dot(self.w_hh, tmp) * (1 - states[i - 1] ** 2)
+        return dw_hh
+
     def backward(self, x: np.ndarray, labels: np.ndarray):
         """
 
@@ -122,16 +166,29 @@ class RNN:
             one_hot_labels = np.zeros(self.vocabulary_size)
             one_hot_labels[labels[t]] = 1
 
-            dy = predicted_probabilities[t] - one_hot_labels
-            dw_hy += np.outer(dy, states[t].T)
+            dy_t = predicted_probabilities[t] - one_hot_labels
+            dh_t = np.dot(self.w_hy.T, dy_t)
+            dz_t_0 = self.calculate_loss_grad_wrt_z(dh_t, states, t)
 
-            dh = np.dot(self.w_hy.T, dy)
+            # dl / dw_hy = (dl / dy) * (dl * dw_hy)
+            dw_hy += np.outer(dy_t, states[t].T)
 
-            dh_dh_ks = []
-            for k in range(t):
-                dh_dz = 1 - states[t] ** 2
-                dz_dh_prev = self.w_hh
-                dh_dh_prev = dh_dz * dz_dh_prev
+            # dl / dw_hh = ∑ (dl / dz_{k}) * (dz_{k} / dw_hy) for all k from 0 to t
+            dw_hh += sum(
+                np.outer(dz_i, states[i - 1]) for dz_i, i in zip(dz_t_0, reversed(range(1, t + 1)))
+            )
+
+            ll = sum(
+                np.(dz_i, states[i - 1]) for dz_i, i in zip(dz_t_0, reversed(range(1, t + 1)))
+            )
+
+            b = self.calculate_w_hh_grad(dh_t, states, t)
+            print(ll.shape, b.shape)
+
+            # dl / dw_hx = ∑ (dl / dz_{k}) * (dz_{k} / dw_hx) for all k from 0 to t
+            dw_hh += sum(
+                np.dot(dz_i, states[i - 1].T) for dz_i, i in zip(dz_t_0, reversed(range(1, t + 1)))
+            )
 
 
 n = 25
