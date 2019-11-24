1d6b6b96b9d8e9e3baec72895b9f0e9aaafc5003
Melikbekyan <razmik.melikbekyan@pmi.com>
2019-11-24 10:54:21 +0100

    docker related changes

--- my_docker_app/my_app.py
+++ my_docker_app/my_app.py
@@ -1,7 +1,9 @@
+import sys
+
 import numpy as np
 
+print(sys.path)
 
-print('aaaaaaaaaaaaaaaaaaaaa')
-
-
-print(np.linspace(0, 100, num=11))
+for _ in range(100):
+    print('aaaaaaaaaaaaaaaaaaaaa')
+    print(np.linspace(0, 100, num=11))
